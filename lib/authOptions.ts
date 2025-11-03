37e5165f849dcc8411fe353d45a0a52a64e02809
soynerd <mesoynerd@gmail.com>
2025-11-04 00:19:56 +0530

    Added Local Auth

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,25 +1,55 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "./prismaClient";
+import bcrypt from "bcryptjs";
+
 export const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
+            clientId: process.env.Google_CLient_ID!,
+            clientSecret: process.env.Google_Client_Secret!,
         }),
-        // ...add more providers here
+        CredentialsProvider({
+            name: "Local Auth",
+            credentials: {
+                email: { type: "email", label: "Email", placeholder: "you@gmail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email && !credentials?.password) throw new Error("Missing Email or Password")
+                try {
+                    const userDetails = await prisma.user.findUnique({
+                        where: { email: credentials?.email }
+                    })
+                    if (!userDetails) {
+                        throw new Error("No user found");
+                    }
+
+                    const isPasswordValid = await bcrypt.compare(credentials!.password, userDetails.hashed_password);
+                    if (isPasswordValid) {
+                        return { id: userDetails.id, name: userDetails.name, email: credentials?.email }
+                    } else {
+                        return null
+                    }
+
+                } catch (error) {
+                    console.error("Provider :: Credetails :: authorize ", error);
+                    throw error;
+                }
+            }
+        })
     ],
     callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            return true
+        async jwt({ token, user }) {
+            if (user) token.id = user.id;
+            return token
         },
 
-        async session({ session, token, user }) {
+        async session({ session, token }) {
+            if (session.user) session.user.id = token.id as string;
             return session
         },
-        async jwt({ token, user, account, profile, isNewUser }) {
-            return token
-        }
     },
     session: {
         strategy: "jwt",
@@ -28,6 +58,7 @@ export const authOptions: NextAuthOptions = {
     pages: {
         error: "/error",
         signOut: "/"
-    }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 
 }
