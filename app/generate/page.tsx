a6bb0cde2a67794d69dcea0d61ddd530725cfdec
soynerd <mesoynerd@gmail.com>
2025-11-04 17:40:26 +0530

    Removed Session Tracking

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,13 +1,7 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import { UpgradeCard } from "@/components/upgrade-card";
 import { GeneratorUI } from "@/components/generator-ui";
-import { getUser } from "@/lib/getUser";
 
 export default async function GeneratePage() {
-  const user = await getUser();
-  if (!user) redirect("/");
-
   return (
     <div className="space-y-8 mx-56">
       <UpgradeCard />
